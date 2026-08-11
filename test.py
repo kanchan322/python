5500c527e482af221591ee924cb78b5487765ad8
kanchan322 <kanchanpaswan731@gmail.com>
2026-08-11 11:36:38 +0545

    my first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+first_name="kanchan"
+last_name="paswan"
+full_name=first_name + " " + last_name
+age= 20
+address="kathmandu"
+print(f"Hello, my name is {full_name}. I am {age} years old and I live in {address}.")
+
+name= input("Enter your name: ")
+city= input("Enter your city: ")
+feeling= input("How are you feeling today? ")
+print()
+print("="*20)
