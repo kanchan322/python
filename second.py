fafb4cb55893b8b1b5e702d86be3eb19aa1101b6
kanchan322 <kanchanpaswan731@gmail.com>
2026-08-12 18:08:43 +0545

    second commit

---
+++ second.py
@@ -0,0 +1,26 @@
+#number=round(3.14159, 1)
+#print(number)
+
+name="kanchan"
+print(type(name))
+
+name="kanchan"
+print(name.title())
+
+name="kanchan"
+print(len(name))
+
+name="kanchan paswan"
+print(name.strip())
+
+sentance="hello world"
+print(sentance.replace("hello", "python"))
+
+sentance="hello world"
+print("hello" in sentance)
+
+name="kanchan"
+print(name[0:3])
+
+
+ 
